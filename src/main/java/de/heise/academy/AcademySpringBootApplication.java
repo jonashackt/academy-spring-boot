ab3ef9767c6c2169ee719ac1ffa501e97a9f3771
Jonas Hecht <jonas.hecht@codecentric.de>
2021-10-21 12:25:47 +0200

    Added Spring Boot application

---
+++ src/main/java/de/heise/academy/AcademySpringBootApplication.java
@@ -0,0 +1,13 @@
+package de.heise.academy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademySpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademySpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/heise/academy/AcademySpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package de.heise.academy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademySpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
